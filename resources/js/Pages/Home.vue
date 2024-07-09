96ba8eb61fa4d0aecd7537a05e72430b206370a5
gregorie536 <vexier.gregorie@gmail.com>
2024-07-09 12:44:50 +0200

    Taille image home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -7,13 +7,15 @@
 
         <!-- Onglets -->
         <div class="flex justify-center gap-4 mb-4">
-            <button class="px-4 py-2 text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600 transition-colors" @click="naviguerVers('/taches')">Tâches</button>
-            <button class="px-4 py-2 text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600 transition-colors" @click="naviguerVers('/presences')">Présences</button>
+            <button class="px-4 py-2 text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600 transition-colors"
+                @click="naviguerVers('/taches')">Tâches</button>
+            <button class="px-4 py-2 text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600 transition-colors"
+                @click="naviguerVers('/presences')">Présences</button>
         </div>
 
         <!-- Image -->
         <div class="flex justify-center">
-            <img src="/img/bredel.webp" alt="Description de l'image" class="max-w-full h-auto">
+            <img src="/img/bredel.webp" alt="Description de l'image" class="max-w-full h-auto img-smaller">
         </div>
     </div>
 </template>
@@ -29,3 +31,10 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.img-smaller {
+    max-width: 75%;
+    height: auto;
+}
+</style>
