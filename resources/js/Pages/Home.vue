d14d484f3a266111e3a748204cc66d1bbd09b4a5
gregorie536 <vexier.gregorie@gmail.com>
2024-07-22 10:18:49 +0200

    Mise à jour du site

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="container mx-auto mt-10 px-4">
-        <!-- Section Titre -->
         <section class="mb-8">
-            <h1 class="text-3xl font-bold text-center text-gray-800">Barbecue Bredel Saison 4</h1>
+            <h1 class="text-3xl font-bold text-center text-gray-800">Barbecue Bredel</h1>
         </section>
 
-        <!-- Onglets -->
-        <div class="flex justify-center gap-4 mb-4">
+        <!-- <div class="flex justify-center gap-4 mb-4">
             <button class="px-4 py-2 text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600 transition-colors"
                 @click="naviguerVers('/taches')">Tâches</button>
             <button class="px-4 py-2 text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600 transition-colors"
                 @click="naviguerVers('/presences')">Présences</button>
-        </div>
-
-        <!-- Image -->
+        </div> -->
+        <section class="mt-10">
+            <h1 class="text-3xl font-bold text-center text-gray-800">Rendez-vous l'année prochaine pour la Saison 5</h1>
+        </section>
         <div class="flex justify-center">
             <img src="/img/bredel.webp" alt="Description de l'image" class="max-w-full h-auto img-smaller">
         </div>
     </div>
+
 </template>
 
 <script>
